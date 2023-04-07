056737511fd1d18711c10f93765e6d68ea1cc80f
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-07 12:41:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-//your JS code here. If required.
+//const form = document.querySelector("#myForm");
+const ageInput = document.querySelector("#age");
+const nameInput = document.querySelector("#name");
+const btn = document.querySelector("#btn");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (ageInput.value.trim() === "" || nameInput.value.trim() === "") {
+    alert("Please fill in all fields.");
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const age = parseInt(ageInput.value);
+      if (age >= 18) {
+        resolve(`Welcome, ${nameInput.value}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${nameInput.value}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  promise.then((result) => {
+    alert(result);
+  }).catch((error) => {
+    alert(error);
+  });
+});
+your JS code here. If required.
+const form = document.querySelector("#myForm");
+const ageInput = document.querySelector("#age");
+const nameInput = document.querySelector("#name");
+const btn = document.querySelector("#btn");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (ageInput.value.trim() === "" || nameInput.value.trim() === "") {
+    alert("Please fill in all fields.");
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const age = parseInt(ageInput.value);
+      if (age >= 18) {
+        resolve(`Welcome, ${nameInput.value}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${nameInput.value}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  promise.then((result) => {
+    alert(result);
+  }).catch((error) => {
+    alert(error);
+  });
+});
